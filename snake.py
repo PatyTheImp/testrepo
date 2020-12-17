90f73f137e7d8334852ec25bd97bfc2784d025b3
PatyTheImp <69981887+PatyTheImp@users.noreply.github.com>
2020-12-17 18:20:28 +0000

    Add files via upload

---
+++ snake.py
@@ -0,0 +1,15 @@
+a = 1
+
+print('PORTUGAL CARALHOOOOO!!!', end=' ')
+print('Patrícia Gonçalves Costa')
+print('a:', a, sep='')
+
+b = abs(-3)
+print(b)
+
+def soma(x, y):
+    return x + y
+
+c = soma(10.8, -5.3)
+
+print(round(c))
